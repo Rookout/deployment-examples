909836e698db781660aded792fe5e16635e8ebf2
Mickael Alliel <mickael.alliel@gmail.com>
2018-03-04 14:39:26 +0200

    Servlet now working with hello world + parameter

--- aws-beanstalk/java-tomcat-elasticbeanstalk/eb-tomcat-helloworld/build.sh
+++ aws-beanstalk/java-tomcat-elasticbeanstalk/eb-tomcat-helloworld/build.sh
@@ -1,9 +1,9 @@
 cd src
 mkdir -p WEB-INF/classes
 echo .
-javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/rookout/web/Default.java
+javac -classpath WEB-INF/lib/*:WEB-INF/classes -d WEB-INF/classes com/rookout/web/HelloWorld.java
 echo .
-jar -cf ROOT.war *.jsp WEB-INF .ebextensions/*.config com
+jar -cf ROOT.war WEB-INF .ebextensions/*.config com
 echo .
 if [ -d "/Library/Tomcat/webapps" ]; then
   cp ROOT.war /Library/Tomcat/webapps
--- aws-beanstalk/java-tomcat-elasticbeanstalk/eb-tomcat-helloworld/src/com/rookout/web/HelloWorld.java
+++ aws-beanstalk/java-tomcat-elasticbeanstalk/eb-tomcat-helloworld/src/com/rookout/web/HelloWorld.java
@@ -5,12 +5,14 @@ import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 import java.io.*;
 
-
 public class HelloWorld extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NumberFormatException {
     String name = request.getParameter("name");
-    request.setAttribute("name", name);
-    RequestDispatcher rd = request.getRequestDispatcher("helloworld.jsp");
-    view.forward(request,response);
+    if (name == null) {
+      name = "world";
+    }   
+    PrintWriter out = response.getWriter();
+    out.println("Hello, " + name + "!");
+    out.close();
   }
 }
