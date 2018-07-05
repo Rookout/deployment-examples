953a52ea3bb25c09fe406e7698a927d34c6d1163
Liran Haimovitch <liran.last@gmail.com>
2018-07-05 19:24:13 +0300

    initial version of example

---
+++ node-ibm-openwhisk/index.js
@@ -0,0 +1,9 @@
+const rookout = require('rookout/openwhisk');
+
+function myAction(args) {
+    const leftPad = require("left-pad")
+    const lines = args.lines || [];
+    return { padded: lines.map(l => leftPad(l, 30, ".")) }
+}
+
+exports.main = rookout.wrap(myAction);
