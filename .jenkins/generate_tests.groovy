0e5dc643e27db7ef610525b2008d57839e695481
Itiel <itielshw@gmail.com>
2018-11-28 18:29:40 +0200

    ""

--- .jenkins/generate_tests.groovy
+++ .jenkins/generate_tests.groovy
@@ -1,12 +1,11 @@
-import groovy.io.FileType
-def list = []
-println("OKOK")
-println("$message")
-def dir = new File("..")
-dir.traverse(type: FileType.DIRECTORIES, maxDepth: 0){ file ->
-  if (file.isDirectory()) {
-  println(file)
-  list << file
-  }
+pipelineJob("/regression-test/$jobName") {
+    properties {
+        disableConcurrentBuilds()
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("$realpwd/$jobName/Jenkinsfile"))
+            sandbox()
+        }
+    }
 }
-script(readFileFromWorkspace('.jenkins/sentry-cron.Jenkinsfile'))
