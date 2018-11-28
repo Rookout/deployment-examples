5c50313a32128bbcab61fbc9b3447c3b01d04171
Itiel <itielshw@gmail.com>
2018-11-28 18:16:20 +0200

    "print things"

--- .jenkins/generate_tests.groovy
+++ .jenkins/generate_tests.groovy
@@ -2,7 +2,7 @@ import groovy.io.FileType
 import org.rookout.files.Utilities
 Utilities.checkIfFileExist(this, 'helm', 'Makefile')
 def list = []
-
+println("OKOK")
 def dir = new File("..")
 dir.traverse(type: FileType.DIRECTORIES, maxDepth: 0){ file ->
   if (file.isDirectory()) {
