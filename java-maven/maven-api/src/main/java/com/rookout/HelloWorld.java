fb74ba72b461e4c142048c3a73091334456c57b4
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-01-23 16:47:20 +0200

    Adding rookout integration with the api

--- java-maven/maven-api/src/main/java/com/rookout/HelloWorld.java
+++ java-maven/maven-api/src/main/java/com/rookout/HelloWorld.java
@@ -3,8 +3,16 @@ import io.javalin.Context;
 import io.javalin.Handler;
 import io.javalin.Javalin;
 
+import com.rookout.rook.API;
+import com.rookout.rook.RookOptions;
+
 public class HelloWorld {
     public static void main(String[] args) {
+
+        RookOptions opts = new RookOptions();
+        opts.token = "d1fee9a4a26620c993fb180677fad4ea6939677b82e6082265f889026f1cd71a";
+        API.start(opts);
+
         Javalin app = Javalin.start(7000);
         app.get("/", new Handler() {
             public void handle(Context ctx) throws Exception {
