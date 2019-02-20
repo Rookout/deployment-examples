0f367c78d5492d584afb7a4ac861f21617a6947e
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-20 18:17:11 +0200

    dasdas

--- java-aws-lambda/awsTest.sh
+++ java-aws-lambda/awsTest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls ; \
 aws lambda create-function \
             --region us-east-2 \
             --function-name java_aws_lambda_test \
@@ -8,6 +9,6 @@ aws lambda create-function \
             --runtime java8 \
 	    --timeout 25 \
 	    --memory-size 400 \
-            --environment Variables="{ROOKOUT_TOKEN=${ROOKOUT_TOKEN},ROOKOUT_ROOK_TAGS=lambda}" ; \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" ; \
             aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
             aws lambda delete-function --region us-east-2 --function-name java_aws_lambda_test 
