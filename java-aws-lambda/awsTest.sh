b1dc71b702e26474162f02d25ece2c0c9ce768ab
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-20 21:42:38 +0200

    Add create funcrtion code to dockerfile

--- java-aws-lambda/awsTest.sh
+++ java-aws-lambda/awsTest.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-ls ; \
-aws lambda create-function \
-            --region us-east-2 \
-            --function-name java_aws_lambda_test \
-            --zip-file fileb://aws-lambda-java-1.0-SNAPSHOT.zip \
-            --role arn:aws:iam::032275105219:role/rookout-lambda-role \
-            --handler TestLambda.TestLambda::handleRequest \
-            --runtime java8 \
-	    --timeout 25 \
-	    --memory-size 400 \
-            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" ; \
-            aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
-            aws lambda delete-function --region us-east-2 --function-name java_aws_lambda_test 
+#!/bin/bash            
+aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" /dev/stdout ; \
+aws lambda delete-function --region us-east-2 --function-name java_aws_lambda_test 
