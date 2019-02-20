2a1257dd200899c8d283c5752bca24af2a71627b
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-20 18:08:08 +0200

    Scripting the right thing

--- java-aws-lambda/awsTest.sh
+++ java-aws-lambda/awsTest.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker run --rm -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e ROOKOUT_TOKEN=$ROOKOUT_TOKEN $1
+aws lambda create-function \
+            --region us-east-2 \
+            --function-name java_aws_lambda_test \
+            --zip-file fileb://aws-lambda-java-1.0-SNAPSHOT.zip \
+            --role arn:aws:iam::032275105219:role/rookout-lambda-role \
+            --handler TestLambda.TestLambda::handleRequest \
+            --runtime java8 \
+	    --timeout 25 \
+	    --memory-size 400 \
+            --environment Variables="{ROOKOUT_TOKEN=${ROOKOUT_TOKEN},ROOKOUT_ROOK_TAGS=lambda}" ; \
+            aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
+            aws lambda delete-function --region us-east-2 --function-name java_aws_lambda_test 
