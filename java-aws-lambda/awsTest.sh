c05160f38c2f0058f6b53fd9e42fdab1ec028f74
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-21 11:43:02 +0200

    Add awsTest

--- java-aws-lambda/awsTest.sh
+++ java-aws-lambda/awsTest.sh
@@ -9,5 +9,7 @@ aws lambda create-function \
 	    --timeout 25 \
 	    --memory-size 400 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" ; \
-aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
-aws lambda delete-function --region us-east-2 --function-name java_aws_lambda_test 
+for run in {1..5}
+do
+  aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
+done
