cd392bba557ac0be0693aaf444d8487dc69fbfe8
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-24 11:59:48 +0200

    Add regression test for node-aws-lambda

---
+++ node-aws-lambda/awsTest.sh
@@ -0,0 +1,14 @@
+aws lambda create-function \
+            --region us-east-2 \
+            --function-name node_aws_lambda_regression_test \
+            --zip-file fileb:///rookout_lambda_test.zip \
+            --role arn:aws:iam::032275105219:role/rookout-lambda-role \
+            --handler lambda_function.lambda_handler \
+            --runtime nodejs8.10 \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" \
+            --timeout 25 ; \
+for run in {1..5}
+do
+  aws lambda invoke --region us-east-2 --function-name node_aws_lambda_regression_test /dev/stdout ; \
+done
+aws lambda delete-function --region us-east-2 --function-name node_aws_lambda_regression_test
