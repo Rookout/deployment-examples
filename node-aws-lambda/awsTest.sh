63c67e6de0c4980b02c34405423aa72e78c9c8b6
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-26 14:20:44 +0200

    Change bashfile to support updating lambda function

--- node-aws-lambda/awsTest.sh
+++ node-aws-lambda/awsTest.sh
@@ -1,14 +1,34 @@
+#list all of the functions and check if the function that im working on right now exists or not
+if aws lambda list-functions --region us-east-2 | grep "node_aws_lambda_regression_test"; then
+aws lambda update-function-code \
+            --function-name node_aws_lambda_regression_test \
+            --zip-file fileb://rookout_lambda_test.zip \
+            --region us-east-2 \
+
+else
 aws lambda create-function \
             --region us-east-2 \
             --function-name node_aws_lambda_regression_test \
-            --zip-file fileb:///../../rookout_lambda_test.zip \
+            --zip-file fileb://rookout_lambda_test.zip \
             --role arn:aws:iam::032275105219:role/rookout-lambda-role \
             --handler index.handler \
             --runtime nodejs8.10 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda,ROOKOUT_LOG_TO_STDERR=1,ROOKOUT_DEBUG=1}" \
             --timeout 25 ; \
-for run in {1..5}
-do
-  aws lambda invoke --region us-east-2 --function-name node_aws_lambda_regression_test /dev/stdout ; \
-done
-aws lambda delete-function --region us-east-2 --function-name node_aws_lambda_regression_test
+fi
+
+
+# aws lambda create-function \
+#             --region us-east-2 \
+#             --function-name node_aws_lambda_regression_test \
+#             --zip-file fileb:///../../rookout_lambda_test.zip \
+#             --role arn:aws:iam::032275105219:role/rookout-lambda-role \
+#             --handler index.handler \
+#             --runtime nodejs8.10 \
+#             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda,ROOKOUT_LOG_TO_STDERR=1,ROOKOUT_DEBUG=1}" \
+#             --timeout 25 ; \
+# for run in {1..5}
+# do
+#   aws lambda invoke --region us-east-2 --function-name node_aws_lambda_regression_test /dev/stdout ; \
+# done
+# aws lambda delete-function --region us-east-2 --function-name node_aws_lambda_regression_test
