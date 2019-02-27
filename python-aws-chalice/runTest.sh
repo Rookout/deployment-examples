81cfc598fccf27315addfa966ab3093e8e7de370
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-27 18:03:39 +0200

    Delete tags

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -12,6 +12,5 @@ else
     aws lambda update-function-configuration \
                 --function-name python_chalice_lambda_regression_test-dev \
                 --region us-east-2 \
-                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" \
-                --tags "type=regression_test" 
+                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
 fi
