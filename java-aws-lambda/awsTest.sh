a07a78edfa26deb1763f5b6cca44eba47cfb2f18
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-27 14:30:50 +0200

    Add reg test to java-aws-lambda

--- java-aws-lambda/awsTest.sh
+++ java-aws-lambda/awsTest.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if aws lambda list-functions --region us-east-2 | grep "java_aws_lambda_test"; then
+aws lambda update-function-code \
+            --function-name java_aws_lambda_test \
+            --zip-file fileb:///aws-lambda-java-1.0-SNAPSHOT.zip \
+            --region us-east-2 \
+
+else
 aws lambda create-function \
             --region us-east-2 \
             --function-name java_aws_lambda_test \
@@ -8,8 +15,5 @@ aws lambda create-function \
             --runtime java8 \
 	    --timeout 25 \
 	    --memory-size 400 \
-            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=lambda}" ; \
-for run in {1..5}
-do
-  aws lambda invoke --region us-east-2 --function-name java_aws_lambda_test /dev/stdout ; \
-done
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=java_aws_lambda_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" ; \
+fi
