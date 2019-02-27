0fde84c05e6cdbb67eb4be6c6232cc7c033835e8
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-27 16:58:34 +0200

    Add token to chalice reg test

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -6,5 +6,9 @@ if aws lambda list-functions --region us-east-2 | grep "python_chalice_lambda_re
                 --region us-east-2 \
 
 else
-    chalice deploy
+    chalice deploy ; \
+    aws lambda update-function-configuration \
+                --function-name python_chalice_lambda_regression_test-dev \
+                --region us-east-2 \
+                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_lambda_regression_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
 fi
