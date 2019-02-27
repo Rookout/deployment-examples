3b955770b56dd3dbafe159a5d25d3d2aa852e789
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-27 17:31:29 +0200

    Add rioght path

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -2,7 +2,7 @@ if aws lambda list-functions --region us-east-2 | grep "python_chalice_lambda_re
     zip -r rookout_lambda_test.zip . -i app.py ; \
     aws lambda update-function-code \
                 --function-name python_chalice_lambda_regression_test \
-                --zip-file fileb:///rookout_lambda_test.zip \
+                --zip-file fileb:///app/rookout_lambda_test.zip \
                 --region us-east-2 \
 
 else
