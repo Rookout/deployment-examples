01e8b0fb72c0e4b8fde2f29a5973818ea0355186
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-27 17:24:25 +0200

    Add things

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -1,5 +1,5 @@
 if aws lambda list-functions --region us-east-2 | grep "python_chalice_lambda_regression_test"; then
-    zip -r rookout_lambda_test.zip app.py ; \
+    zip -r rookout_lambda_test.zip . -i app.py ; \
     aws lambda update-function-code \
                 --function-name python_chalice_lambda_regression_test \
                 --zip-file fileb:///rookout_lambda_test.zip \
