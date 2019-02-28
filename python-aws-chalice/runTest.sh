1d616dcf40070966fca21f9056fdf1cb0b6218c5
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-28 15:28:42 +0200

    Add sleep time

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -14,6 +14,6 @@ else
                 --function-name python_chalice_lambda_regression_test-dev \
                 --region us-east-2 \
                 --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
-    sleep 10
+    sleep 20
     curl ${RESULT[26]}
 fi
