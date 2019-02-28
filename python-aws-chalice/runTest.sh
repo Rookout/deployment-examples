becb5b97f527e300b415ea38c19bc3b4758cc818
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-28 13:44:23 +0200

    Add right configuration

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -1,16 +1,14 @@
 if aws lambda list-functions --region us-east-2 | grep "python_chalice_lambda_regression_test"; then
-    cd /app ; \
     zip -r rookout_lambda_test.zip app.py ; \
     aws lambda update-function-code \
-                --function-name python_chalice_lambda_regression_test \
+                --function-name python_chalice_lambda_regression_test-dev \
                 --zip-file fileb:///rookout_lambda_test.zip \
-                --region us-east-2 \
+                --region us-east-2 
 
 else
-    cd /app ; \
     chalice deploy ; \
     aws lambda update-function-configuration \
                 --function-name python_chalice_lambda_regression_test-dev \
                 --region us-east-2 \
-                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
+                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
 fi
