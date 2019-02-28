419934df97262d35788d1d8d37111eaef19330e2
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-28 16:05:23 +0200

    Add sleep time

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -1,20 +1,13 @@
 #!bin/bash
 cd /var/task
-if aws lambda list-functions --region us-east-2 | grep "python_chalice_lambda_regression_test-dev"; then
-    zip -r rookout_lambda_test.zip app.py ; \
-    aws lambda update-function-code \
-                --function-name python_chalice_lambda_regression_test-dev \
-                --zip-file fileb:///rookout_lambda_test.zip \
-                --region us-east-2 
+declare RESULT=($(chalice deploy)) ; \
+for i in “${RESULT[@]}” do echo " " + %i done 
+aws lambda update-function-configuration \
+            --function-name python_chalice_lambda_regression_test-dev \
+            --region us-east-2 \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
+sleep 20
+curl ${RESULT[26]}
+sleep 60
+aws lambda delete-function --region us-east-2 --function-name python_chalice_lambda_regression_test-dev
 
-else
-    declare RESULT=($(chalice deploy)) ; \
-    for i in “${RESULT[@]}” do echo " " + %i done 
-    aws lambda update-function-configuration \
-                --function-name python_chalice_lambda_regression_test-dev \
-                --region us-east-2 \
-                --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
-    sleep 20
-    curl ${RESULT[26]}
-    aws lambda delete-function --region us-east-2 --function-name python_chalice_lambda_regression_test-dev
-fi
