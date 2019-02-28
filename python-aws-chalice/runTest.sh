89172dad4fcf8910906f29c7feca19c896a3d1ab
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-02-28 16:33:34 +0200

    Add more time

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -8,6 +8,6 @@ aws lambda update-function-configuration \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
 sleep 20
 curl ${RESULT[26]}
-sleep 140
+sleep 300
 aws lambda delete-function --region us-east-2 --function-name python_chalice_lambda_regression_test-dev
 
