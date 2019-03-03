9bdb22349e5995bd0839ea6194a18970da2ba6f8
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-03 16:12:38 +0200

    Add more sleep time before curl

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -5,7 +5,7 @@ aws lambda update-function-configuration \
             --function-name python_chalice_lambda_regression_test-dev \
             --region us-east-2 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
-sleep 20
+sleep 40
 #https://github.com/aws/chalice/issues/490
 curl ${RESULT[26]}
 sleep 140
