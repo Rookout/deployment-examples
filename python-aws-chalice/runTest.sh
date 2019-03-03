9e0f25ee5a16305c681706929eb9366af2d87005
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-03 16:43:13 +0200

    Typo

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -7,9 +7,10 @@ aws lambda update-function-configuration \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
 sleep 40
 #https://github.com/aws/chalice/issues/490
-for run in {1..5} do 
-    curl ${RESULT[26]}
-    sleep 5
+for run in {1..5} 
+do 
+    curl ${RESULT[26]} ; \
+    sleep 5 ;
 done
 sleep 140
 aws lambda delete-function --region us-east-2 --function-name python_chalice_lambda_regression_test-dev
