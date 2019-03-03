1528491107b5c78b6aaf54cda44b3dc753c32bbb
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-03 15:29:43 +0200

    Add issue to bashfile on chalice

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -6,6 +6,7 @@ aws lambda update-function-configuration \
             --region us-east-2 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
 sleep 20
+#https://github.com/aws/chalice/issues/490
 curl ${RESULT[26]}
 sleep 80
 aws lambda delete-function --region us-east-2 --function-name python_chalice_lambda_regression_test-dev
