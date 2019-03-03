fcf2c86e02a7557f0a290bc739ccd427c580c49d
orishoshan <ori.shoshan@rookout.com>
2019-03-03 13:43:00 +0200

    Various small fixups

--- python-spark/example.py
+++ python-spark/example.py
@@ -1,7 +1,10 @@
 from pyspark.sql.types import StructType, StructField, FloatType, StringType, IntegerType
+from pyspark.context import SparkContext
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, udf
+
+import time
 
 schema = StructType([
     StructField("partition", IntegerType()),
@@ -33,32 +36,39 @@ def get_address_info(_):
             "country": "USA"}
 
 
+def handle_record(record):
+    record = record.asDict()
+    time.sleep(1)
+    record.update(get_address_info(record))
+    return record
+
+
 def map_partitions_handler(records_iterator):
     records_with_address_info = []
     for record in records_iterator:
-        record = record.asDict()
-        record.update(get_address_info(record))
-        records_with_address_info.append(record)
+        records_with_address_info.append(handle_record(record))
     return records_with_address_info
 
 
 def main():
-    import rook
-    rook.start()
 
     spark_session = SparkSession.builder \
         .enableHiveSupport() \
         .getOrCreate()
+    print "Test"
     with open("sample-data.csv", "wb") as sample_data_file:
         sample_data_file.write("lat,long\n")
-        for i in xrange(100000):
+        for i in xrange(30):
             sample_data_file.write("%d,40.714224,-73.961452\n" % (i % 10))
     points = spark_session.read.csv("sample-data.csv", header=True, schema=schema).repartition(10)
     points_rdd = points.rdd.mapPartitions(map_partitions_handler)
     points = spark_session.createDataFrame(data=points_rdd, schema=points.schema, verifySchema=False)
     points = add_udf_column(points)
+    points = points.cache()
     print points.take(1)
 
 
 if __name__ == "__main__":
+    import rook
+    rook.start()
     main()
