19aedf2bb97f82db3a389bcec5eb08bb20a12bd7
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-11 11:48:17 +0200

    Add right path

--- java-aws-elasticbeanstalk/runTest.sh
+++ java-aws-elasticbeanstalk/runTest.sh
@@ -1,6 +1,6 @@
 aws s3 cp /app/java-elastic-beanstalk.zip s3://java-elastic-beanstalk-regression-test ; \
 now="$(date +'%d-%m-%Y-%H:%M:%S')" ; \
 aws elasticbeanstalk create-application-version --application-name regression-tests --version-label $now --source-bundle S3Bucket="java-elastic-beanstalk-regression-test",S3Key="java-elastic-beanstalk.zip" ; \
-aws elasticbeanstalk update-environment --application-name regression-tests --environment-name java-aws-elasticbeanstalk --version-label $now --option-settings file:///options.json ; \
+aws elasticbeanstalk update-environment --application-name regression-tests --environment-name java-aws-elasticbeanstalk --version-label $now --option-settings file:///app/options.json ; \
 sleep 40 ; \
 curl http://java-aws-elasticbeanstalk.us-east-2.elasticbeanstalk.com/
