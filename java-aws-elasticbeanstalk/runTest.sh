e5feb691621da5735b91c758be9255256e336cbb
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-11 15:48:42 +0200

    Add all the environment variables

--- java-aws-elasticbeanstalk/runTest.sh
+++ java-aws-elasticbeanstalk/runTest.sh
@@ -1,6 +1,6 @@
 aws s3 cp /app/java-elastic-beanstalk.zip s3://java-elastic-beanstalk-regression-test ; \
 now="$(date +'%d-%m-%Y-%H:%M:%S')" ; \
 aws elasticbeanstalk create-application-version --application-name regression-tests --version-label $now --source-bundle S3Bucket="java-elastic-beanstalk-regression-test",S3Key="java-elastic-beanstalk.zip" ; \
-aws elasticbeanstalk update-environment --application-name regression-tests --environment-name java-aws-elasticbeanstalk --version-label $now --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN ; \
+aws elasticbeanstalk update-environment --application-name regression-tests --environment-name java-aws-elasticbeanstalk --version-label $now --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN,OptionName=ROOKOUT_ROOK_TAGS,Value="http://java-aws-elasticbeanstalk",OptionName=ROOKOUT_DEBUG,Value=1 ; \
 sleep 40 ; \
 curl http://java-aws-elasticbeanstalk.us-east-2.elasticbeanstalk.com/
