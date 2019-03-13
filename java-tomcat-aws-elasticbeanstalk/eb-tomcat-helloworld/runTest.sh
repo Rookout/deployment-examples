527f2e8a4841576cbd8637b4d462c535f2830e82
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-13 18:33:01 +0200

    Adding test to tomcat

---
+++ java-tomcat-aws-elasticbeanstalk/eb-tomcat-helloworld/runTest.sh
@@ -0,0 +1,14 @@
+aws s3 cp /ROOT.war s3://java-tomcat-elastic-beanstalk-reg-test
+
+now="$(date +'%d-%m-%Y-%H:%M:%S')"
+
+aws elasticbeanstalk create-application-version \
+                                --application-name regression-tests \
+                                --version-label $now \
+                                --source-bundle S3Bucket="java-tomcat-elastic-beanstalk-reg-test",S3Key="ROOT.war" ; \
+
+aws elasticbeanstalk update-environment \
+                            --application-name regression-tests \
+                            --environment-name java-tomcat-aws-elasticbeanstalk \
+                            --version-label $now \
+                            --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN,OptionName=ROOKOUT_ROOK_TAGS,Value="java-aws-elasticbeanstalk",OptionName=ROOKOUT_DEBUG,Value=1
