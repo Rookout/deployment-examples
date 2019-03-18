e3c2b4a75de7523eb7186e5f271f73ca30294fe0
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-18 11:22:37 +0200

    Delete projcet

--- java-tomcat-aws-elasticbeanstalk/eb-tomcat-helloworld/runTest.sh
+++ java-tomcat-aws-elasticbeanstalk/eb-tomcat-helloworld/runTest.sh
@@ -13,4 +13,4 @@ aws elasticbeanstalk update-environment \
                             --application-name regression-tests \
                             --environment-name java-tomcat-aws-elasticbeanstalk \
                             --version-label $now 
-                            # --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN,OptionName=ROOKOUT_ROOK_TAGS,Value="java-tomcat-aws-elasticbeanstalk",OptionName=ROOKOUT_DEBUG,Value=1
+                            --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN,OptionName=ROOKOUT_ROOK_TAGS,Value="java-tomcat-aws-elasticbeanstalk",OptionName=ROOKOUT_DEBUG,Value=1
