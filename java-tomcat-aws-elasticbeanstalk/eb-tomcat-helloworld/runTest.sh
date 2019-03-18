b544258ca4c587cb34b6d07ad4dc2c2148d2443a
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-18 14:13:36 +0200

    Add uuid

--- java-tomcat-aws-elasticbeanstalk/eb-tomcat-helloworld/runTest.sh
+++ java-tomcat-aws-elasticbeanstalk/eb-tomcat-helloworld/runTest.sh
@@ -1,15 +1,15 @@
 cd /app
 aws s3 cp java-aws-tomcat-test.zip s3://java-tomcat-elastic-beanstalk-reg-test
 
-now="$(date +'%d-%m-%Y-%H:%M:%S')"
-
+version=$(uuidgen)
+echo version
 aws elasticbeanstalk create-application-version \
                                 --application-name regression-tests \
-                                --version-label $now \
+                                --version-label $version \
                                 --source-bundle S3Bucket="java-tomcat-elastic-beanstalk-reg-test",S3Key="java-aws-tomcat-test.zip" ; \
 
 aws elasticbeanstalk update-environment \
                             --application-name regression-tests \
                             --environment-name java-tomcat-aws-elasticbeanstalk \
-                            --version-label $now \
+                            --version-label $version \
                             --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=ROOKOUT_TOKEN,Value=$ROOKOUT_TOKEN,OptionName=ROOKOUT_ROOK_TAGSValue="java-tomcat-aws-elasticbeanstalk",OptionName=ROOKOUT_DEBUG,Value=1
