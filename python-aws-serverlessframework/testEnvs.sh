e46d46ea960bbb3619acfb1105320d3e716085a5
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-20 14:30:19 +0200

    Add environment variables

--- python-aws-serverlessframework/testEnvs.sh
+++ python-aws-serverlessframework/testEnvs.sh
@@ -1,2 +1,7 @@
 cd /app 
+
 sls deploy
+
+aws lambda update-function-configuration \
+            --function-name python-aws-serverlessframework-dev-hello \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python-aws-serverlessframework-dev-hello,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
