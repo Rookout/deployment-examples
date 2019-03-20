50ab2b7ff2b07e0adb4c5e8c57e0fd6922451bc0
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-20 15:52:43 +0200

    Add docekr to dockerfile

--- python-aws-serverlessframework/testEnvs.sh
+++ python-aws-serverlessframework/testEnvs.sh
@@ -3,5 +3,5 @@ cd /app
 sls deploy
 
 aws lambda update-function-configuration \
-            --function-name python-aws-serverlessframework-dev-hello \
-            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python-aws-serverlessframework-dev-hello,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
+            --function-name python-aws-serverlessframework-regression-test-dev-hello \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python-aws-serverlessframework-regression-test-dev-hello,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
