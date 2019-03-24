a629cfbc39ab21824df858fa74a8902bddea5056
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-24 14:55:27 +0200

    Add dockerfile.test

--- node-serverless-cross-platform/runTest.sh
+++ node-serverless-cross-platform/runTest.sh
@@ -1,5 +1,5 @@
 sls deploy
 
 aws lambda update-function-configuration \
-            --function-name serverless-app-dev-hello \
+            --function-name aws-nodejs-dev-example \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python-aws-serverlessframework-regression-test-dev-hello,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
