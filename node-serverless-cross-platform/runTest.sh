861fcac7454ff15e3d20cbf20c783abe0ac5b367
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-25 15:15:43 +0200

    Adding a few fixes

--- node-serverless-cross-platform/runTest.sh
+++ node-serverless-cross-platform/runTest.sh
@@ -3,5 +3,5 @@ cd /app
 sls deploy --force
 
 aws lambda update-function-configuration \
-            --function-name node-serverless-regression-test-dev \
-            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-serverless-regression-test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
+            --function-name node-serverless-regression-test-dev-example \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-	node-serverless-regression-test-dev-example,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
