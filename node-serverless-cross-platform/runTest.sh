c01a6676063a36b9c13b8fe0d4b5ebba115dd386
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-26 13:57:46 +0200

    Add to new lambda function

--- node-serverless-cross-platform/runTest.sh
+++ node-serverless-cross-platform/runTest.sh
@@ -3,5 +3,5 @@ cd /app
 sls deploy --force
 
 aws lambda update-function-configuration \
-            --function-name node-serverless-regression-test-dev-example \
-            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-serverless-regression-test-dev-example,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
+            --function-name node-serverless-reg-test-dev-example \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-serverless-reg-test-dev-example,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
