b434895ed7d6adf9ae3a85f3ec939713b6df560c
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-26 15:05:08 +0200

    Add sleep time

--- node-serverless-cross-platform/runTest.sh
+++ node-serverless-cross-platform/runTest.sh
@@ -6,4 +6,6 @@ aws lambda update-function-configuration \
             --function-name node-serverless-reg-test-dev-example \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-serverless-reg-test-dev-example,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
 
+sleep 15
+
 sls remove 
