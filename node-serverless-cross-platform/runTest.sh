5e1752590ab31657bfc2439671f42dfb524fd72d
Manuel Nemirovsky <manuel.nemirovsky@gmail.com>
2019-03-26 18:47:54 +0200

    RK-1949 , RK-1999 - Manuel/node serverless test (#109)
    * Add test
    
    * Orginizing test
    
    * Add test
    
    * Add specific test
    
    * Add serverless app name
    
    * Adding dockerfile test
    
    * Add dockerfile.test
    
    * Add gitignore file
    
    * Add other ignore files
    
    * Add other ignore files
    
    * Add environment varaible2
    
    * Delete changes
    
    * Add different base image
    
    * Delte line in jenkinsfile
    
    * Delete Jenkinsfile
    
    * Add right path to serverless
    
    * Add right function name
    
    * Change dockerignore
    
    * Add dorce to daply
    
    * Adding a few fixes
    
    * Update tags
    
    * Add right code
    
    * Add to new lambda function
    
    * Add minor fix's
    
    * Add sleep time
    
    * Add deploy after remove
    
    * Fix bashfile
    
    * Checking force deployment
    
    * Add regular service name
    
    * Change file name

---
+++ node-serverless-cross-platform/runTest.sh
@@ -0,0 +1,7 @@
+cd /app
+
+sls deploy --force
+
+aws lambda update-function-configuration \
+            --function-name node-serverless-reg-test-dev-example \
+            --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node-serverless-reg-test-dev-example,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
