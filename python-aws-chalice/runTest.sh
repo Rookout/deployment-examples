bfb9cdae2c5c3283a96fa95ab60a1934fd18a2bb
Itiel <itielshw@gmail.com>
2019-04-15 19:46:51 +0300

    even more changes

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -11,10 +11,11 @@ echo "Sleeping for 20 secs"
 sleep 20
 echo "Triggering the lambda/ checking to see the result" 
 curl ${RESULT[26]}
-echo "Sleeping for 60 seconds more" 
-sleep 60
 echo "Trying to get logs" 
 chalice logs index
 chalice logs python_chalice_lambda_regression_test-dev
+echo "Sleeping for 60 seconds more" 
+sleep 60
 echo "Deleting the stage" 
 chalice delete --stage dev
+echo "DONEEE"
