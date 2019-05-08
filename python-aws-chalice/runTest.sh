ae5db4c615bef9cbd485b1f3652dc27b00c1b0af
Mickael Alliel <mickael@rookout.com>
2019-05-08 11:45:39 +0300

    More time for chalice

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -7,15 +7,15 @@ aws lambda update-function-configuration \
             --function-name python_chalice_lambda_regression_test-dev \
             --region us-east-2 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
-echo "Sleeping for 20 secs" 
-sleep 20
+echo "Sleeping for 35 secs" 
+sleep 35
 echo "Triggering the lambda/ checking to see the result" 
 URL=$(echo $RESULT | grep -oE 'Rest API URL: (.+\/api\/)' | cut -d\  -f4)
 curl $URL
 echo "Trying to get logs" 
 chalice logs  --name index
 chalice logs  --name python_chalice_lambda_regression_test-dev
-echo "Sleeping for 60 seconds more" 
+echo "Sleeping for 20 seconds more" 
 sleep 20
 echo "Deleting the stage" 
 chalice delete --stage dev
