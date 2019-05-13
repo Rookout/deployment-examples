f00b922393c6178a9ac3a596aa423efe6c8981ef
Mickael Alliel <mickael@rookout.com>
2019-05-13 14:12:44 +0300

    try new method of triggering

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -2,17 +2,19 @@
 cd /var/task
 echo "Deploying the lambda, and update with envs" 
 RESULT=$(chalice deploy) ; echo $RESULT ; \
-echo "Sleeping for 20 secs"
-sleep 20
+echo "Sleeping for 10 secs"
+sleep 10
 aws lambda update-function-configuration \
             --function-name python_chalice_lambda_regression_test-dev \
             --region us-east-2 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" 
-echo "Sleeping for 20 secs" 
+echo "Sleeping for 20 secs"
 sleep 20
-echo "Triggering the lambda/ checking to see the result" 
+echo "Fetching URL to trigger"
 URL=$(echo $RESULT | grep -oE 'Rest API URL: (.+\/api\/)' | cut -d\  -f4)
-curl $URL
+echo "URL_TO_TRIGGER -- START"
+echo $URL
+echo "URL_TO_TRIGGER -- END"
 echo "Trying to get logs" 
 chalice logs  --name index
 chalice logs  --name python_chalice_lambda_regression_test-dev
