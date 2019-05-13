c85771e72f68cea6697cb4d3235aa0ed7ad773aa
Mickael Alliel <mickael@rookout.com>
2019-05-13 21:14:48 +0300

    chalice

--- python-aws-chalice/runTest.sh
+++ python-aws-chalice/runTest.sh
@@ -13,8 +13,8 @@ aws lambda update-function-configuration \
             --function-name python_chalice_lambda_regression_test-dev \
             --region us-east-2 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_chalice_lambda_regression_test-dev,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}"
-echo "Sleeping for 150 seconds more"
-sleep 150
+echo "Sleeping for 240 seconds more"
+sleep 240
 echo "Trying to get logs" 
 chalice logs  --name index
 chalice logs  --name python_chalice_lambda_regression_test-dev
