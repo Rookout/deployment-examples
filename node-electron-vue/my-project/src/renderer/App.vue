d47d92729e5113282e79cd6aac0aee7dd3d0f53b
BENROOKOUT\Ben <ben@rookout.com>
2019-08-04 17:39:11 +0300

    adding electron vue example

---
+++ node-electron-vue/my-project/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'node-electron-vue-rookout-example'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
