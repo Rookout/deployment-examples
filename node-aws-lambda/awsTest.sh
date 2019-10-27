86a6761bdd29a59a43aa443fff1314cd8440a012
Mickael Alliel <mickael@rookout.com>
2019-10-27 16:18:32 +0200

    Use updated rook according to job (#154)

--- node-aws-lambda/awsTest.sh
+++ node-aws-lambda/awsTest.sh
@@ -1,14 +1,18 @@
+cd /build
+npm install ${JENKINS_AGENT_WORKDIR}/${rookNodeVersion}.tgz
+zip -r /dist/rookout_lambda_test_regression.zip .
+
 if aws lambda list-functions --region us-east-2 | grep "node_aws_lambda_regression_test"; then
 aws lambda update-function-code \
             --function-name node_aws_lambda_regression_test \
-            --zip-file fileb:///../../rookout_lambda_test.zip \
+            --zip-file fileb:///dist/rookout_lambda_test_regression.zip \
             --region us-east-2 ; \
  
 else
 aws lambda create-function \
             --region us-east-2 \
             --function-name node_aws_lambda_regression_test \
-            --zip-file fileb:///../../rookout_lambda_test.zip \
+            --zip-file fileb:///dist/rookout_lambda_test_regression.zip \
             --role arn:aws:iam::032275105219:role/rookout-lambda-role \
             --handler index.handler \
             --runtime nodejs8.10 \
