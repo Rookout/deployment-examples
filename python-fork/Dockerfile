d0c0f7d2a3bbb6b506ef2db97527c7aa6a2a17e5
Ben Klein <36033708+BenTheKlein@users.noreply.github.com>
2020-07-05 10:29:23 +0300

    python fork (#178)

---
+++ python-fork/Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.7-stretch
+ADD requirements.txt .
+RUN pip install -r requirements.txt
+ADD fork_rookout.py .
---
+++ python-fork/fork_rookout.py
@@ -0,0 +1,38 @@
+import time
+from flask import Flask
+from random import randint
+import os
+from datetime import datetime
+import rook
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    time.sleep(0.01 * randint(10, 200) + 0.1)
+    return 'Index Main Page'
+
+@app.route('/hello')
+def hello():
+    time.sleep(0.01 * randint(10, 200) + 0.1)
+    return 'Hello, World'  # Set bp here
+
+
+def debug_here(i):
+    j = i * i
+    return j  # Set bp here
+
+
+def child_routine():
+    for i in range(10):
+        debug_here(i)
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    rook.start(throw_errors=True, fork=True)
+    if 0 == os.fork():
+        app.run(host="0.0.0.0", port=5000, threaded=True)
+    else:
+        child_routine()
