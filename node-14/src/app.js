6b3e58e0349c2f208251c2b6b783c0395936735f
yuvalshavit <yuval@rookout.com>
2020-08-04 15:21:56 +0300

    Add Node 14 regression test
    https://rookout.atlassian.net/browse/RK-6589

---
+++ node-14/src/app.js
@@ -0,0 +1,12 @@
+const rookout = require('rookout');
+rookout.start();
+
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => res.send("Hello World"));
+app.get("/hello/:name", (req, res) => {
+    res.send("Hello, " + req.params.name);
+});
+
+app.listen(5000);
