c5f8801c598718627e8585ba2eae15e62332d981
Dudi Cohen <31986166+ElDuderinos@users.noreply.github.com>
2021-07-11 18:31:43 +0300

    RK-9848 - fix preceeding "," instead of a space in AWS tests (#252)
    * fix preceeding "," instead of a space
    
    * fix node versions
    
    * fixing node versions for aws
    
    * back to node 10
    
    * update node runtime

--- node-aws-lambda/awsTest.sh
+++ node-aws-lambda/awsTest.sh
@@ -15,7 +15,7 @@ aws lambda create-function \
             --zip-file fileb:///dist/rookout_lambda_test_regression.zip \
             --role arn:aws:iam::032275105219:role/rookout-lambda-role \
             --handler index.handler \
-            --runtime nodejs8.10 \
+            --runtime nodejs10.x \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=node_aws_lambda_regression_test,ROOKOUT_LOG_TO_STDERR=1,ROOKOUT_DEBUG=1}" \
             --timeout 25 \
             --tags "type=regression_test" ; \
