43953c28b12aad05735ca69cc67b6555fc50db08
benklein <ben@rookout.com>
2021-08-30 13:22:09 +0300

    a

---
+++ node-typescript-inline-sources/src/another_file.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+"use strict";
+export function add(x: number, y: number): number {
+  return x + y;
+}
--- node-typescript-inline-sources/src/app.ts
+++ node-typescript-inline-sources/src/app.ts
@@ -7,9 +7,11 @@ rookout.start();
 const express = require("express");
 const app = express();
 
+import { add } from "./another_file"
+
 app.get('/', (req: any, res: any) => res.send("Hello World"));
 app.get("/hello/:name", (req: any, res: any) => {
-    res.send("Hello, " + req.params.name);
+    res.send("Hello, " + req.params.name +" :: " + add(5,4));
 });
 
 app.listen(5000);
