6f0efec11341c20496599a45d71387eca8e86d5a
benklein <ben@rookout.com>
2021-09-26 21:13:37 +0300

    update to 3.7

--- python-aws-lambda/awsTest.sh
+++ python-aws-lambda/awsTest.sh
@@ -11,7 +11,7 @@ aws lambda create-function \
             --zip-file fileb:///rookout_lambda_test.zip \
             --role arn:aws:iam::032275105219:role/rookout-lambda-role \
             --handler lambda_function.lambda_handler \
-            --runtime python2.7 \
+            --runtime python3.7 \
             --environment Variables="{ROOKOUT_TOKEN=$ROOKOUT_TOKEN,ROOKOUT_ROOK_TAGS=python_lambda_regression_test,ROOKOUT_DEBUG=1,ROOKOUT_LOG_TO_STDERR=1}" \
             --timeout 25 \
             --memory-size 400 \
