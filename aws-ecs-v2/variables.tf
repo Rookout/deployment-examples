c484241337805065c84f0c6f8ba0eeff57c4d0dc
Mor Yakobovich <morya994@gmail.com>
2021-11-22 18:03:10 +0200

    working datastore

--- aws-ecs-v2/variables.tf
+++ aws-ecs-v2/variables.tf
@@ -6,9 +6,12 @@ variable "subnet_id" {
   description = "Subnet ID"
   type        = string
 }
-
 variable "rookout_token" {
   description = "Token of your Rookout organization"
   type        = string
 }
-
+variable "datastore_server_mode" {
+  description = "Server mode (AUTOTLS/TLS/PLAIN) for Rookout Datastore"
+  type        = string
+  default     = "PLAIN"
+}
