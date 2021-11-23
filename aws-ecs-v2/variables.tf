96d047861d0da9085e14aaeb8e87999a9412de28
Mor Yakobovich <morya994@gmail.com>
2021-11-23 16:09:45 +0200

    more variables + readme

--- aws-ecs-v2/variables.tf
+++ aws-ecs-v2/variables.tf
@@ -6,16 +6,20 @@ variable "subnet_id" {
   description = "Subnet ID"
   type        = string
 }
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
 variable "rookout_token" {
   description = "Token of your Rookout organization"
+  type        = string
+}
+variable "cluster_name" {
+  description = "Cluster Name"
   type        = string
 }
 variable "datastore_server_mode" {
   description = "Server mode (AUTOTLS/TLS/PLAIN) for Rookout Datastore"
   type        = string
   default     = "PLAIN"
-}
-variable "cluster_name" {
-  description = "Cluster Name"
-  type        = string
 }
