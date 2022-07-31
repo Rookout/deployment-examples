11f584f7209b00c23ac088627620ffa2387cd1b0
Guy Gazit <guy@rookout.com>
2022-07-31 18:37:06 +0300

    other side of the noderook's tracing_support

--- node-14/src/app.js
+++ node-14/src/app.js
@@ -1,12 +1,42 @@
+const tracer = require('dd-trace').init({ service: "bix-test-ms", env: "none" });
+tracer.use('express');
+
+const ah = require('async_hooks');
+
 const rookout = require('rookout');
 rookout.start();
 
 const express = require("express");
 const app = express();
 
+class O {
+    constructor(s) {
+        this.s = s;
+    }
+}
+
+function includes(s) {
+    return true;
+}
+
 app.get('/', (req, res) => res.send("Hello World"));
 app.get("/hello/:name", (req, res) => {
-    res.send("Hello, " + req.params.name);
+    console.log("@@@@@@@@@@@@@@@@@@");
+    console.log(ah.executionAsyncId());
+    // const x = tracer;
+    // console.log(tracer.scope().active());
+    // console.log(ah.executionAsyncResource());
+    // let s = Symbol('ddResourceStore');
+    // console.log(Symbol('ddResourceStore') === s);
+    // console.log(ah.executionAsyncResource()[s]);
+    let f = false;
+    let r = new O("hello");
+    const a = undefined;
+    var b = 9;
+    let c = undefined;
+    var d = undefined;
+    const name = req.params.name + req.params.name;
+    res.send("Hello, " + name);
 });
 
-app.listen(5000);
+app.listen(9898);
