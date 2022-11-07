0de5616a305d5683460de43b8176ffc1aaf7d525
Dudi Cohen <31986166+ElDuderinos@users.noreply.github.com>
2022-11-07 14:13:10 +0200

    RK-14688 - Dynamic loading script (#316)
    * fix ebs namespace options
    
    * change dynamic loading to use a script

---
+++ java-gradle/java-dynamic-loading/runTest.sh
@@ -0,0 +1,5 @@
+unset JAVA_TOOL_OPTIONS
+export JAVA_TOOL_OPTIONS=""
+java -jar /build/libs/rookoutDemo-1.0.0.jar &
+export ROOKOUT_TARGET_PID=$(ps -ef | grep 'java -jar /build/libs/rookoutDemo-1.0.0.jar' | grep -v grep | awk '{print $2}') 
+java -jar /home/jenkins/agent/rook.jar &
