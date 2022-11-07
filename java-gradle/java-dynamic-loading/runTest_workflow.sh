c04eb4aff719672cfee8f77d7758f2c2c9a4670e
Gosha <gosha@rookout.com>
2022-11-07 16:01:40 +0200

    regTest artifact for workflows

--- java-gradle/java-dynamic-loading/runTest_workflow.sh
+++ java-gradle/java-dynamic-loading/runTest_workflow.sh
@@ -2,4 +2,4 @@ unset JAVA_TOOL_OPTIONS
 export JAVA_TOOL_OPTIONS=""
 java -jar /build/libs/rookoutDemo-1.0.0.jar &
 export ROOKOUT_TARGET_PID=$(ps -ef | grep 'java -jar /build/libs/rookoutDemo-1.0.0.jar' | grep -v grep | awk '{print $2}') 
-java -jar /home/jenkins/agent/rook-all.jar &
+java -jar $ROOK_DEST/rook-all.jar &
